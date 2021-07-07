df564d5b34b2776bdf2e78bf21a6d90bb330e60d
rafael-tivane <luislangabusiness@gmail.com>
2021-07-07 22:36:58 +0100

    Adding new section to the home page

---
+++ src/components/SeeProductLink/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+const SeeProductLink = ({ url, customClass }) => {
+    return (
+        <Link to={url} className={`uppercase text-white decoration-none link see-product-link ${customClass}`}>
+            See product
+        </Link>
+    );
+};
+
+export default SeeProductLink;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,21 @@
+import SeeProductLink from '../../components/SeeProductLink';
+import './styles.css';
+
 const Home = () => {
     return (
-        <div>Hello</div>
+        <main>
+            <section className="align-center flex flex-column justify-center bg-center no-repeat bg-cover
+                width-100 hero">
+                <div className="align-center flex flex-column justify-center text-center text-white  width-100 px-5">
+                    <h1 className="hero__title">XX99 Mark II HeadphoneS</h1>
+                    <p className="hero__description">
+                        Experience natural, lifelike audio and exceptional build quality made 
+                        for the passionate music enthusiast.
+                    </p>
+                    <SeeProductLink url="/" customClass="hero__link" />
+                </div>
+            </section>
+        </main>
     );
 };
 
