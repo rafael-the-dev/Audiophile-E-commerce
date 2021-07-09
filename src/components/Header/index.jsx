e5eb5787668aeec235c2f643d22aad2a18f8c2f1
rafael-tivane <luislangabusiness@gmail.com>
2021-07-09 10:52:02 +0100

    Adding descriptive classes to the page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className="flex px-5 width-100 header">
+        <header className="flex px-5 width-100 px-lg header">
             <div className="align-center flex justify-between width-100 header__division">
                 <div className="align-center flex justify-start header__division-2">
                     <button
                         className="fa fa-bars fa-icon background-transparent border-none outline-none
-                        text-white header__menu-button"
+                        text-white d-none-md header__menu-button"
                         aria-label="Menu button">
                     </button>
-                    <Logo customClass="logo--tablet"/>
+                    <Logo customClass="d-none d-block-tablet logo--tablet"/>
                 </div>
-                <Logo customClass="logo__header-mobile" />
-                <nav className="d-none header__navigation">
+                <Logo customClass="logo__header-mobile d-none-tablet" />
+                <nav className="d-none header__navigation d-flex-md">
                     <ul className="align-center flex justify-between header__list">
                         <li className="header__item">
                             <Link to="/" className="block uppercase text-white header__link">Home</Link>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <main>
             <section className="align-center flex flex-column justify-center bg-center no-repeat bg-cover
-                align-start-desktop px-tablet px-desktop width-100 hero">
+                align-start-desktop px-tablet px-lg width-100 hero">
                 <div className="align-center flex flex-column justify-center text-center text-white 
                     align-start-desktop width-100 px-5 hero__division">
                     <h1 className="hero__title">XX99 Mark II<br/>HeadphoneS</h1>
