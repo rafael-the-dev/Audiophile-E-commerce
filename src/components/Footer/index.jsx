a050adacd9d67978cbcad2e7e7bf4829670028aa
rafael-tivane <luislangabusiness@gmail.com>
2021-07-13 22:24:50 +0100

    Adding a responsive footer to the page

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,55 @@
+import { Link } from 'react-router-dom';
+import Logo from '../Logo';
+import './styles.css';
+
+const Footer = () => {
+    return (
+        <footer className="align-center flex flex-column relative justify-between px-5 px-lg footer
+        align-start-sm">
+            <div className="absolute footer__bar"></div>
+            <div className="align-center flex flex-column justify-between w-100
+                align-start-sm flex-row-md">
+                <Logo customClass="footer__logo"/>
+                <nav className="footer__navigation">
+                    <ul className="align-center flex flex-column justify-between footer__list
+                        flex-row-sm">
+                        <li className="footer__item">
+                            <Link to="/" className="block uppercase text-white footer__link">Home</Link>
+                        </li>
+                        <li className="footer__item">
+                            <Link to="/" className="block uppercase text-white footer__link">Headphone</Link>
+                        </li>
+                        <li className="footer__item">
+                            <Link to="/" className="block uppercase text-white footer__link">Speakers</Link>
+                        </li>
+                        <li className="footer__item">
+                            <Link to="/" className="block uppercase text-white footer__link">Earphones</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <p className="text-white text-center footer__description">
+                Audiophile is an all in one stop to fulfill your audio needs. We're a small team of 
+                music lovers and sound specialists who are devoted to helping you get the most out 
+                of personal audio. Come and visit our demo facility - we’re open 7 days a week.
+            </p>
+            <div className="align-center flex flex-column justify-center footer__division w-100
+                flex-row-sm justify-between-sm">
+                <p className="text-white footer__copyright">Copyright 2021. All Rights Reserved</p>
+                <div className="align-center flex justify-center footer__social-media">
+                    <Link to="/" className="footer__social-media-link">
+                        <i className="d-block bg-center no-repeat bg-contain footer__icon footer__icon--facebook"></i>
+                    </Link>
+                    <Link to="/" className="footer__social-media-link">
+                        <i className="d-block bg-center no-repeat bg-contain footer__icon footer__icon--twitter"></i>
+                    </Link>
+                    <Link to="/" className="footer__social-media-link">
+                        <i className="d-block bg-center no-repeat bg-contain footer__icon footer__icon--instagram"></i>
+                    </Link>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,6 +10,7 @@ import Card from '../../components/Card';
 import H1 from '../../components/H1';
 import H2 from '../../components/H2';
 import { useEffect, useRef } from 'react';
+import Footer from '../../components/Footer';
 
 const Home = () => {
     const earphoneRef = useRef(null);
@@ -36,80 +37,83 @@ const Home = () => {
         });
     }, []);
     return (
-        <main>
-            <section className="align-center flex flex-column justify-center bg-center no-repeat bg-cover
-                align-start-desktop px-tablet px-lg width-100 hero">
-                <div className="align-center flex flex-column justify-center text-center text-white 
-                    align-start-desktop width-100 px-5 hero__division">
-                    <H1 customClass="hero__title" text="XX99 Mark II"><br/>HeadphoneS</H1>
-                    <p className="hero__description">
-                        Experience natural, lifelike audio and exceptional build quality made 
-                        for the passionate music enthusiast.
-                    </p>
-                    <SeeProductLink url="/" customClass="hero__link" />
-                </div>
-            </section>
-            <section className="flex flex-column px-5 width-100 align-stretch-sm flex-row-sm
-                justify-between-sm px-lg products-cards">
-                <ProductCart url="/" name="Headphones" image={headphones}/>
-                <ProductCart url="/" name="Speakers" image={speakers}/>
-                <ProductCart url="/" name="Earphones" image={earsphones}/>
-            </section>
-            
-            <section className="px-5 px-lg">
-                <div className="align-start flex justify-center flex-column bg-center no-repeat
-                    bg-cover width-100 px-5 speaker-section">
-                    <H2 customClass="speaker-section__title" text="ZX7 SPEAKER" />
-                    <SeeProductLink url="/" customClass="text-black secondary" />
-                </div>
-            </section>
+        <>
+            <main>
+                <section className="align-center flex flex-column justify-center bg-center no-repeat bg-cover
+                    align-start-desktop px-tablet px-lg width-100 hero">
+                    <div className="align-center flex flex-column justify-center text-center text-white 
+                        align-start-desktop width-100 px-5 hero__division">
+                        <H1 customClass="hero__title" text="XX99 Mark II"><br/>HeadphoneS</H1>
+                        <p className="hero__description">
+                            Experience natural, lifelike audio and exceptional build quality made 
+                            for the passionate music enthusiast.
+                        </p>
+                        <SeeProductLink url="/" customClass="hero__link" />
+                    </div>
+                </section>
+                <section className="flex flex-column px-5 width-100 align-stretch-sm flex-row-sm
+                    justify-between-sm px-lg products-cards">
+                    <ProductCart url="/" name="Headphones" image={headphones}/>
+                    <ProductCart url="/" name="Speakers" image={speakers}/>
+                    <ProductCart url="/" name="Earphones" image={earsphones}/>
+                </section>
+                
+                <section className="px-5 px-lg">
+                    <div className="align-start flex justify-center flex-column bg-center no-repeat
+                        bg-cover width-100 px-5 speaker-section">
+                        <H2 customClass="speaker-section__title" text="ZX7 SPEAKER" />
+                        <SeeProductLink url="/" customClass="text-black secondary" />
+                    </div>
+                </section>
 
-            <section className="px-5 px-lg align-stretch-sm d-flex-sm flex-row-sm justify-center-sm
-                earphones-section">
-                <figure className="width-100 earphones-section__image-container">
-                    <img
-                        ref={earphoneRef}
-                        src={earphone}
-                        alt="YX1 EARPHONES"
-                        className="d-block height-100 width-100"
-                    />
-                </figure>
-                <div className="align-start flex justify-center flex-column 
-                    width-100 px-5 earphones-section__content">
-                    <H2 customClass="earphones-section__content-title" text="YX1 EARPHONES" />
-                    <SeeProductLink url="/" customClass="others" />
-                </div>
-            </section>
+                <section className="px-5 px-lg align-stretch-sm d-flex-sm flex-row-sm justify-center-sm
+                    earphones-section">
+                    <figure className="width-100 earphones-section__image-container">
+                        <img
+                            ref={earphoneRef}
+                            src={earphone}
+                            alt="YX1 EARPHONES"
+                            className="d-block height-100 width-100"
+                        />
+                    </figure>
+                    <div className="align-start flex justify-center flex-column 
+                        width-100 px-5 earphones-section__content">
+                        <H2 customClass="earphones-section__content-title" text="YX1 EARPHONES" />
+                        <SeeProductLink url="/" customClass="others" />
+                    </div>
+                </section>
 
-            <section className="align-center flex flex-column px-5 width-100 px-lg justify-between-md 
-                align-stretch-md best-gear">
-                <figure className="width-100 best-gear__image-container">
-                    <img
-                        ref={bestGearRef}
-                        src={bestGear}
-                        alt=""
-                        className="d-block height-100 width-100"
-                    />
-                </figure>
-                <div className="text-center px-5 best-gear__content">
-                    <H2
-                        customClass="uppercase best-gear__content-title"
-                        text="Bringing you the"
-                    >
-                        <em className='best-gear__content-title--highlight'>best</em>
-                        audio gear
-                    </H2>
-                    <p className="best-gear__content-description">
-                        Located at the heart of New York City, Audiophile is the premier store for 
-                        high end headphones, earphones, speakers, and audio accessories. We have a 
-                        large showroom and luxury demonstration rooms available for you to browse and 
-                        experience a wide range of our products. Stop by our store to meet some of the 
-                        fantastic people who make Audiophile the best place to buy your portable audio 
-                        equipment.
-                    </p>
-                </div>
-            </section>
-        </main>
+                <section className="align-center flex flex-column px-5 width-100 px-lg justify-between-md 
+                    align-stretch-md best-gear">
+                    <figure className="width-100 best-gear__image-container">
+                        <img
+                            ref={bestGearRef}
+                            src={bestGear}
+                            alt=""
+                            className="d-block height-100 width-100"
+                        />
+                    </figure>
+                    <div className="text-center px-5 best-gear__content">
+                        <H2
+                            customClass="uppercase best-gear__content-title"
+                            text="Bringing you the"
+                        >
+                            <em className='best-gear__content-title--highlight'>best</em>
+                            audio gear
+                        </H2>
+                        <p className="best-gear__content-description">
+                            Located at the heart of New York City, Audiophile is the premier store for 
+                            high end headphones, earphones, speakers, and audio accessories. We have a 
+                            large showroom and luxury demonstration rooms available for you to browse and 
+                            experience a wide range of our products. Stop by our store to meet some of the 
+                            fantastic people who make Audiophile the best place to buy your portable audio 
+                            equipment.
+                        </p>
+                    </div>
+                </section>
+            </main>
+            <Footer />
+        </>
     );
 };
 
